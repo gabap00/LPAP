83c907d16577d1789ccb585613c3c4d45fbddf4e
Gabriel Pereira <rm88451@estudante.fieb.edu.br>
2023-08-30 09:07:35 -0300

    Turista

---
+++ Viagem2H/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Classe Objeto         Construtor
+            Turista mochileiro = new Turista();
+
+//Definir formato do objeto
+        mochileiro.setNome("Lindsey Lohan");
+        mochileiro.cpf =(Validacao.cpf("123");
+        Turista mochileira = new Turista("Maria");
+        mochileiro.setNome("james Gun");
+
+       String retorno = mochileiro.viajar();
+        System.out.println(retorno);
+    }
+
+}
